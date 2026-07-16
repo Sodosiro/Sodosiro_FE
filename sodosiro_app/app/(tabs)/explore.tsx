2bbae635d19b915d7760986cb4d08eccc416de0b
jjjuni <0210leejun@gmail.com>
2026-07-17 00:04:43 +0900

    ♻️ refactor: 바텀바 수정 및 리팩토링

--- sodosiro_app/app/(tabs)/explore.tsx
+++ sodosiro_app/app/(tabs)/explore.tsx
@@ -1,7 +1,6 @@
-import { View } from "react-native-reanimated/lib/typescript/Animated";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function HomeScreen() {
+export default function ExploreScreen() {
   return (
     <SafeAreaView className={`flex-1`}>
     </SafeAreaView>

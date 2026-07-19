551b66c9b8b588135c33d69fc4996ed9dba1f1a0
jjjuni <0210leejun@gmail.com>
2026-07-20 00:12:02 +0900

    💄 style: 아이콘 추가 및 경로 수정

--- sodosiro_app/assets/svgs/index.ts
+++ sodosiro_app/assets/svgs/index.ts
@@ -2,37 +2,46 @@
 export { default as PlaceHeartIcon } from './place_heart.svg';
 
 // 16px
-export { default as RemoveMiniIcon } from './remove_mini.svg';
-export { default as InfoMiniIcon } from './info_mini.svg';
-export { default as NavigationMiniIcon } from './navigation_mini.svg';
-export { default as PinMiniIcon } from './pin_mini.svg';
-export { default as RefreshMiniIcon } from './refresh_mini.svg';
-export { default as ShareMiniIcon } from './share_mini.svg';
-export { default as AwardIcon } from './award.svg';
-export { default as BigCheckIcon } from './big-check.svg';
+export { default as RemoveMiniIcon } from './16px/remove_mini.svg';
+export { default as InfoMiniIcon } from './16px/info_mini.svg';
+export { default as NavigationMiniIcon } from './16px/navigation_mini.svg';
+export { default as PinMiniIcon } from './16px/pin_mini.svg';
+export { default as RefreshMiniIcon } from './16px/refresh_mini.svg';
+export { default as ShareMiniIcon } from './16px/share_mini.svg';
 
 // 24px
-export { default as UpIcon } from './up.svg';
-export { default as DownIcon } from './down.svg';
-export { default as LeftIcon } from './chevron-left.svg';
-export { default as RightIcon } from './chevron-right.svg';
-export { default as NotificationIcon } from './notification.svg';
-export { default as SearchIcon } from './search.svg';
-export { default as RefreshIcon } from './refresh.svg';
-export { default as CheckIcon } from './check.svg';
-export { default as ChipIcon } from './chip.svg';
-export { default as XIcon } from './x.svg';
-export { default as CalendarIcon } from './calendar.svg';
-export { default as RemoveIcon } from './remove.svg';
+export { default as UpIcon } from './24px/up.svg';
+export { default as DownIcon } from './24px/down.svg';
+export { default as LeftIcon } from './24px/chevron-left.svg';
+export { default as RightIcon } from './24px/chevron-right.svg';
+export { default as NotificationIcon } from './24px/notification.svg';
+export { default as SearchIcon } from './24px/search.svg';
+export { default as RefreshIcon } from './24px/refresh.svg';
+export { default as CheckIcon } from './24px/check.svg';
+export { default as AwardIcon } from './24px/award.svg';
+export { default as ChipIcon } from './24px/chip.svg';
+export { default as BigCheckIcon } from './24px/big-check.svg';
+export { default as XIcon } from './24px/x.svg';
+export { default as CalendarIcon } from './24px/calendar.svg';
+export { default as RemoveIcon } from './24px/remove.svg';
 
-export { default as SpinnerIcon } from './spinner.svg';
+export { default as SpinnerIcon } from './24px/spinner.svg';
 
 // 32px
-export { default as CarIcon } from './car.svg';
-export { default as BusIcon } from './bus.svg';
-export { default as HeartCircle } from './heart_circle.svg';
-export { default as HeartCircleStroke } from './heart_circle_stroke.svg';
-export { default as BigXIcon } from './big_x.svg';
+export { default as CarIcon } from './32px/car.svg';
+export { default as BusIcon } from './32px/bus.svg';
+export { default as HeartCircle } from './32px/heart_circle.svg';
+export { default as HeartCircleStroke } from './32px/heart_circle_stroke.svg';
+export { default as BigXIcon } from './32px/big_x.svg';
 
 // 10px
-export { default as DotIcon } from './dot.svg';
+export { default as DotIcon } from './10px/dot.svg';
+
+// category
+export { default as ActivityIcon } from "./category/activity.svg";
+export { default as AttractionIcon } from "./category/attraction.svg";
+export { default as CafeIcon } from "./category/cafe.svg";
+export { default as NatureIcon } from "./category/nature.svg";
+export { default as RestaurantIcon } from "./category/restaurant.svg";
+export { default as ShoppingIcon } from "./category/shopping.svg";
+export { default as CultureIcon } from "./category/culture.svg";

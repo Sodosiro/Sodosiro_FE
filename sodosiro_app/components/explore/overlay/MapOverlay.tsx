d30758fd9ad89182f1e9758897a46aa68eb50095
jjjuni <0210leejun@gmail.com>
2026-07-21 04:13:07 +0900

    ✨ feat: 맵, 오버레이 UI 구현

---
+++ sodosiro_app/components/explore/overlay/MapOverlay.tsx
@@ -0,0 +1,40 @@
+import { View } from "react-native";
+import SearchBar from "./SearchBar";
+import CategoryList from "../../common/CategoryList";
+import PlaceLegend from "./PlaceLegend";
+import ClearSearchButton from "./ClearSearchButton";
+import { router, useLocalSearchParams } from "expo-router";
+import { useState } from "react";
+
+export default function MapOverlay() {
+  const { keyword: param } = useLocalSearchParams<{
+    keyword?: string;
+  }>();
+
+  const [selectedCategory, setSelectedCategory] = useState<CategoryType>("all");
+
+  return (
+    <View className={`flex-1`}>
+      <View>
+        <View className={`px-5 py-3`}>
+          <SearchBar keyword={param} />
+        </View>
+        <CategoryList
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+          paddingHorizontal={20}
+        />
+      </View>
+
+      <View className={`flex-1`}>
+        <PlaceLegend className={`absolute bottom-0 left-5`} />
+        {param && (
+          <ClearSearchButton
+            className={`absolute bottom-0 self-center`}
+            onPress={() => router.push("/(tabs)/explore")}
+          />
+        )}
+      </View>
+    </View>
+  );
+}
---
+++ sodosiro_app/components/explore/overlay/SearchBar.tsx
@@ -0,0 +1,37 @@
+import { RemoveIcon, SearchIcon } from "@/assets/svgs";
+import { Pressable } from "react-native";
+import CustomText from "../../common/CustomText";
+import { router } from "expo-router";
+
+export default function SearchBar({ keyword }: { keyword?: string }) {
+  return (
+    <Pressable
+      className={`flex-row justify-between items-center border border-border bg-white w-full px-6 h-15 rounded-full`}
+      onPress={() => {
+        router.push({
+          pathname: "/explore/search",
+          params: {
+            keyword: keyword ?? "",
+          },
+        });
+      }}
+    >
+      <CustomText
+        className={`${keyword ? `text-text-primary` : `text-text-secondary`} text-title-tight flex-1 h-4`}
+      >
+        {keyword?.trim() ? keyword : "가고 싶은 여행지를 검색해보세요"}
+      </CustomText>
+      {keyword ? (
+        <RemoveIcon
+          color={"#888888"}
+          onPress={() => {
+            router.replace("/(tabs)/explore");
+            router.push("/explore/search");
+          }}
+        />
+      ) : (
+        <SearchIcon color={"#888888"} />
+      )}
+    </Pressable>
+  );
+}

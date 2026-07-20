18c8f95aa7d8416b4769802eedc3a6d1cb7d25b8
sun-kyo <psk910903@gmail.com>
2026-07-20 23:40:47 +0900

    ✨ feat: 서브 타이틀 컴포넌트 구현

---
+++ sodosiro_app/components/common/Subtitle.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from 'react-native';
+
+type Props = {
+  title: string;
+  description?: string;
+};
+
+export default function Subtitle({ title, description }: Props) {
+  return (
+    <View className="flex-row items-end gap-2">
+      <Text className="text-[18px] font-bold text-[#1A1A1A]">{title}</Text>
+
+      {description && <Text className="mb-[2px] text-[14px] font-medium text-[#888888]">{description}</Text>}
+    </View>
+  );
+}

584877ae72b7efc8275ab3e0dd86cea29c8d1496
sun-kyo <psk910903@gmail.com>
2026-07-21 17:39:17 +0900

    💄 style: CustomButton 사이즈 속성 추가

--- sodosiro_app/components/common/CustomButton.tsx
+++ sodosiro_app/components/common/CustomButton.tsx
@@ -1,58 +1,77 @@
-import { Pressable, Text, View, ViewStyle } from "react-native";
-import Animated, { interpolateColor, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
-import Spinner from "./Spinner";
 import { BigCheckIcon } from "@/assets/svgs";
+import { Pressable, Text, View, ViewStyle } from "react-native";
+import Animated, {
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+import Spinner from "./Spinner";
 
 type ButtonProps = {
-  type: 'primary' | 'secondary' | 'tertiary';
+  type: "primary" | "secondary" | "tertiary";
   title: string;
-  size?: 'small' | 'medium' | 'large';
+  size?: "small" | "medium" | "large" | "full";
   disabled?: boolean;
   checked?: boolean;
   loading?: boolean;
-}
+  onPress?: () => void;
+};
 
 const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 
 export default function CustomButton({
   type,
   title,
-  size = 'large',
+  size = "large",
   checked = false,
   disabled = false,
   loading = false,
+  onPress,
 }: ButtonProps) {
-
   const pressed = useSharedValue(0);
 
-  const PrimaryColor = ["#C4D96A", "#A9C92D"]
-  const SecondaryColor = ["#1A1A1A", "#3D3D3D"]
-  const TertiaryColor = ["#F5F5F5", "#E2E2E8"]
+  const PrimaryColor = ["#C4D96A", "#A9C92D"];
+  const SecondaryColor = ["#1A1A1A", "#3D3D3D"];
+  const TertiaryColor = ["#F5F5F5", "#E2E2E8"];
 
   const animatedStyle = useAnimatedStyle(() => ({
-    backgroundColor: disabled ? "#F4F4F4"
+    backgroundColor: disabled
+      ? "#F4F4F4"
       : interpolateColor(
-        pressed.value,
-        [0, 1],
-        type === 'primary' ? PrimaryColor : type === 'secondary' ? SecondaryColor : TertiaryColor,
-      ),
+          pressed.value,
+          [0, 1],
+          type === "primary" ? PrimaryColor : type === "secondary" ? SecondaryColor : TertiaryColor,
+        ),
   }));
 
-  const primaryButtonSize = size === 'large' ? 'w-[320px] h-13' : size === 'medium' ? 'w-[235px] h-13' : 'self-start px-4 h-11';
-  const secondaryButtonSize = 'self-start px-4 h-11';
-  const tertiaryButtonSize = 'self-start px-4 h-11'
+  const primaryButtonSize =
+    size === "full"
+      ? "w-full h-13"
+      : size === "large"
+        ? "w-[320px] h-13"
+        : size === "medium"
+          ? "w-[235px] h-13"
+          : "self-start px-4 h-11";
+  const secondaryButtonSize = "self-start px-4 h-11";
+  const tertiaryButtonSize = "self-start px-4 h-11";
 
-  const textSize = type === 'primary' && size !== 'small' ? 'text-title-tight' : 'text-body3-tight'
+  const textSize = type === "primary" && size !== "small" ? "text-title-tight" : "text-body3-tight";
 
-  const textClass = disabled ? 'text-text-muted' : type === 'secondary' ? 'text-btn-secondary-text' : 'text-text-primary'
-  const textColor = disabled ? '#888888' : type === 'secondary' ? "#FFFFFF" : "#1A1A1A"
+  const textClass = disabled
+    ? "text-text-muted"
+    : type === "secondary"
+      ? "text-btn-secondary-text"
+      : "text-text-primary";
+  const textColor = disabled ? "#888888" : type === "secondary" ? "#FFFFFF" : "#1A1A1A";
 
-  const borderClass = 'border border-border'
+  const borderClass = "border border-border";
 
   return (
     <AnimatedPressable
+      onPress={onPress}
       className={`
-        flex justify-center items-center ${type === 'primary' ? primaryButtonSize : type === 'secondary' ? secondaryButtonSize : tertiaryButtonSize} text-title-tight rounded-full ${type === 'tertiary' ? borderClass : ``}`}
+        flex justify-center items-center ${type === "primary" ? primaryButtonSize : type === "secondary" ? secondaryButtonSize : tertiaryButtonSize} text-title-tight rounded-full ${type === "tertiary" ? borderClass : ``}`}
       disabled={disabled || loading}
       onPressIn={() => {
         pressed.value = withTiming(1, {
@@ -64,16 +83,16 @@ export default function CustomButton({
           duration: 100,
         });
       }}
-      style={animatedStyle as ViewStyle}>
+      style={animatedStyle as ViewStyle}
+    >
       {!loading ? (
         <View className={`flex flex-row justify-center items-center gap-1`}>
           {checked && <BigCheckIcon color={textColor} />}
           <Text className={`${textSize} ${textClass}`}>{title}</Text>
         </View>
       ) : (
-        <Spinner color={type === 'secondary' ? "#FFFFFF" : "#1A1A1A"}/>
+        <Spinner color={type === "secondary" ? "#FFFFFF" : "#1A1A1A"} />
       )}
-
     </AnimatedPressable>
-  )
+  );
 }

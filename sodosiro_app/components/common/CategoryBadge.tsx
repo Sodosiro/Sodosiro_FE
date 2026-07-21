141cc59663638be704f4ca1711e3500564bee177
sun-kyo <psk910903@gmail.com>
2026-07-21 13:07:31 +0900

    ✨ feat: 카테고리 뱃지 컴포넌트에 onPress 속성 추가

--- sodosiro_app/components/common/CategoryBadge.tsx
+++ sodosiro_app/components/common/CategoryBadge.tsx
@@ -1,18 +1,27 @@
-import { ActivityIcon, AttractionIcon, CafeIcon, CultureIcon, NatureIcon, RestaurantIcon, ShoppingIcon } from "@/assets/svgs";
+import {
+  ActivityIcon,
+  AttractionIcon,
+  CafeIcon,
+  CultureIcon,
+  NatureIcon,
+  RestaurantIcon,
+  ShoppingIcon,
+} from "@/assets/svgs";
 import { Pressable, Text } from "react-native";
 
 type Props = {
   disabled?: boolean;
   selected?: boolean;
   category: CategoryType;
-}
+  onPress?: () => void;
+};
 
 export default function CategoryBadge({
   disabled = false,
   selected = false,
   category,
+  onPress,
 }: Props) {
-
   const IconMap = {
     activity: ActivityIcon,
     attraction: AttractionIcon,
@@ -31,17 +40,23 @@ export default function CategoryBadge({
     restaurant: "식당",
     shopping: "쇼핑",
     culture: "문화",
-  }
+  };
 
-  const Icon = IconMap[category]
-  const text = CategoryTextMap[category]
+  const Icon = IconMap[category];
+  const text = CategoryTextMap[category];
 
   return (
-    <Pressable className={`${selected ? `bg-[#1A1A1A]` : disabled ? `bg-btn-disabled` : `bg-white`} flex-row items-center self-start px-4 py-2.5 gap-1 rounded-full border border-border`} disabled={disabled}>
-      <Icon color={selected ? "white" : disabled ? "#888888" : "#1A1A1A"}/>
-      <Text className={`${selected ? `text-white` : disabled ? `text-text-muted` : `text-[#1A1A1A]`}`}>
+    <Pressable
+      className={`${selected ? `bg-[#1A1A1A]` : disabled ? `bg-btn-disabled` : `bg-white`} flex-row items-center self-start px-4 py-2.5 gap-1 rounded-full border border-border`}
+      disabled={disabled}
+      onPress={onPress}
+    >
+      <Icon color={selected ? "white" : disabled ? "#888888" : "#1A1A1A"} />
+      <Text
+        className={`${selected ? `text-white` : disabled ? `text-text-muted` : `text-[#1A1A1A]`}`}
+      >
         {text}
       </Text>
     </Pressable>
-  )
+  );
 }

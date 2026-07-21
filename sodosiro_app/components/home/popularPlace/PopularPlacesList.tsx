12ec3c6f4c72031b813a2ba8e5543a044dd30133
jjjuni <0210leejun@gmail.com>
2026-07-21 20:53:32 +0900

    ♻️ refactor: 코드 리팩토링

--- sodosiro_app/components/home/popularPlace/PopularPlacesList.tsx
+++ sodosiro_app/components/home/popularPlace/PopularPlacesList.tsx
@@ -7,12 +7,11 @@ export default function PopularPlacesList() {
   return (
     <View className={`gap-4 mt-1`}>
       {HomePopularPlaces.map((place, index) => (
-        <View className={`flex-row items-center justify-between`}>
+        <View key={index} className={`flex-row items-center justify-between`}>
           <CustomText font="body3" className={`w-4 text-text-muted`}>
             {index + 1}
           </CustomText>
           <PlaceMini
-            key={place.id}
             imageSource={place.imageSource}
             title={place.title}
             desc={place.desc}

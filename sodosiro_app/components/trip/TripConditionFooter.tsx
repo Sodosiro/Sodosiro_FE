5695cb41776a119d36818fe8de2cb2c71996a269
sun-kyo <psk910903@gmail.com>
2026-07-22 15:47:27 +0900

    ♻️ refactor: TripConditionFooter 리팩토링

--- sodosiro_app/components/trip/TripConditionFooter.tsx
+++ sodosiro_app/components/trip/TripConditionFooter.tsx
@@ -1,7 +1,7 @@
 import { RefreshIcon } from "@/assets/svgs"; // 재설정 아이콘 경로 확인 필요
 import CustomButton from "@/components/common/CustomButton"; // 경로 확인 필요
-import { View } from "react-native";
 import AnimatedButton from "../common/AnimatedButton";
+import BottomActionFooter from "../common/BottomActionFooter";
 import CustomText from "../common/CustomText";
 
 type Props = {
@@ -12,24 +12,22 @@ type Props = {
 
 export default function TripConditionFooter({ onReset, onSubmit, disabled = false }: Props) {
   return (
-    <View className={`px-5 py-3 bg-white border-t-[0.5] border-border`}>
-      <View className={`flex-row gap-2`}>
-        <AnimatedButton
-          className={`flex-row items-center justify-center px-4 gap-1 rounded-full`}
-          backgroundColor={["#FFFFFF", "#F5F5F5"]}
-        >
-          <RefreshIcon width={16} />
-          <CustomText font="body1">재설정</CustomText>
-        </AnimatedButton>
-        <CustomButton
-          type="primary"
-          title="일정 짜기"
-          stretch
-          size="medium"
-          disabled={disabled}
-          onPress={onSubmit}
-        />
-      </View>
-    </View>
+    <BottomActionFooter>
+      <AnimatedButton
+        className={`flex-row items-center justify-center px-4 gap-1 rounded-full`}
+        backgroundColor={["#FFFFFF", "#F5F5F5"]}
+      >
+        <RefreshIcon width={16} />
+        <CustomText font="body1">재설정</CustomText>
+      </AnimatedButton>
+      <CustomButton
+        type="primary"
+        title="일정 짜기"
+        stretch
+        size="medium"
+        disabled={disabled}
+        onPress={onSubmit}
+      />
+    </BottomActionFooter>
   );
 }

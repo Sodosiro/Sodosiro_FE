edce4bb2f0a52eb7b504af261e234afa0a2ab03d
jjjuni <0210leejun@gmail.com>
2026-07-30 19:19:44 +0900

    ♻️ refactor: 코드 리팩토링

--- sodosiro_app/components/bottombar/AnimatedTabIcon.tsx
+++ sodosiro_app/components/bottombar/AnimatedTabIcon.tsx
@@ -1,5 +1,4 @@
 import useSelectedAnimation from "@/hooks/useSelcetedAnimation";
-import { useEffect } from "react";
 
 type Props = {
   focused: boolean;
@@ -12,7 +11,6 @@ export default function AnimatedTabIcon({ focused, Icon }: Props) {
   const { strokeStyle } = useSelectedAnimation(focused, {
     color: COLOR,
   });
-  useEffect(() => console.log("mounted"), []);
 
   return <Icon width={24} height={24} animatedProps={strokeStyle} />;
 }

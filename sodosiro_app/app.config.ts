e28502c38294ed7d14ededb33e4cb46859d6f97a
jjjuni <0210leejun@gmail.com>
2026-09-03 15:08:06 +0900

    🔖 version: release v1.0.1

--- sodosiro_app/app.config.ts
+++ sodosiro_app/app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "소도시로",
   slug: "sodosiro",
-  version: "1.0.0",
+  version: "1.0.1",
   orientation: "portrait",
   icon: "./assets/images/logo.png",
   scheme: "sodosiro",
